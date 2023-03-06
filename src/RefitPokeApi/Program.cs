da739966d8441e24a5420a331135c9eb76bbbb9e
Jaime Fernando Neis <jaime.neis.ext@e-ai.com.br>
2023-03-06 20:22:25 -0300

    Projeto worker criado

---
+++ src/RefitPokeApi/Program.cs
@@ -0,0 +1,10 @@
+using PokeApi;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
